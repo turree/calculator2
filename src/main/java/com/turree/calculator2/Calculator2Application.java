20913df20344864cb20ceca017cb0f74703fa666
turre <turre69@outlook.com>
2022-09-28 20:53:27 +0300

    Add Spring Boot skeleton

---
+++ src/main/java/com/turree/calculator2/Calculator2Application.java
@@ -0,0 +1,13 @@
+package com.turree.calculator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/turree/calculator2/Calculator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turree.calculator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
